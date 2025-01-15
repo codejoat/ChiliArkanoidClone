97b71f62b52cc354faf8bf8958f061450a796e2f
Christopher Lawyer <joat1979@gmail.com>
2025-01-15 12:23:42 -0600

    Added RectF for handling rectangles

---
+++ Engine/RectF.cpp
@@ -0,0 +1,29 @@
+#include "RectF.h"
+
+RectF::RectF (float set_left, float set_right, float set_top, float set_bottom)
+	:
+	left(set_left),
+	right(set_right),
+	top(set_top),
+	bottom(set_bottom)
+{
+}
+
+RectF::RectF (const Vec2& top_left, const Vec2& bottom_right)
+{
+	RectF (top_left.x, bottom_right.x, top_left.y, bottom_right.y);
+}
+
+RectF::RectF (const Vec2& top_left, float (width), float (height))
+{
+	RectF (top_left, Vec2 (width, height));
+}
+
+bool RectF::IsOverlapping (const RectF& other) const
+{
+	return
+		right > other.left &&
+		left < other.right &&
+		bottom > other.top &&
+		top < other.bottom;
+}
---
+++ Engine/RectF.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Vec2.h"
+
+class RectF
+{
+public:
+	RectF () = default;
+	RectF (float set_left, float set_right, float set_top, float set_bottom);
+	RectF (const Vec2& top_left, const Vec2& bottom_right);
+	RectF (const Vec2& top_left, float (width), float(height));
+	bool IsOverlapping (const RectF& other) const;
+public:
+	float left;   // refers to top left of x
+	float right;  // refers to bottom right of x
+	float top;    // refers to top left of y
+	float bottom; // refers to bottom right of y
+};
+
